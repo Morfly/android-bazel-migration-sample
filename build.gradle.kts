efa29fcc9b50f86e4dbecdaeebc49e84059c139b
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2021-08-18 18:09:16 +0300

    updated gradle scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,13 @@
 buildscript {
-    val kotlinVersion by extra("1.5.10")
-
     repositories {
         google()
         mavenCentral()
     }
-
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.android.tools.build:gradle:7.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
     }
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        jcenter() // Warning: this repository is going to shut down soon
-    }
-}
-
 
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
