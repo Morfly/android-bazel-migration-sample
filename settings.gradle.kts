dfaef5f6e2d089aa34cbb1889cdb8d3e8f61ab23
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2021-08-18 18:09:16 +0300

    updated gradle scripts

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "android-migration-sample"
+include(":app", ":images")
